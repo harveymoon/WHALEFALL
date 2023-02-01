249c3062c69837041f2fb358b5348f4b7300af51
harveymoon <harveymoon@me.com>
2023-01-31 21:33:58 -0800

    remove poem, update timeline

--- sketch.js
+++ sketch.js
@@ -36,22 +36,22 @@ var poem = [
 ]
 
 var poemLines = []
-  
+
 var fontRegular;
 
 function preload() {
   fontRegular = loadFont('font/Rajdhani-Light.ttf');
 
-if(random()>.5){
-  Wimg = loadImage("img/whalefall_01-2@0.25x.png");
-  Bimg = loadImage("img/whalefall_01bones_.png");
-}else{
-Wimg = loadImage("img/v2_post@0.25x.png");
-   Bimg = loadImage("img/v2_bonez.png");
-}
+  if (random() > .5) {
+    Wimg = loadImage("img/whalefall_01-2@0.25x.png");
+    Bimg = loadImage("img/whalefall_01bones_.png");
+  } else {
+    Wimg = loadImage("img/v2_post@0.25x.png");
+    Bimg = loadImage("img/v2_bonez.png");
+  }
 
 
-   
+
   timeData = loadJSON("timeData.json")
 }
 
@@ -107,78 +107,78 @@ function setup() {
   noStroke();
   textWrap(WORD);
 
-  for(let pl = 0; pl < poem.length; pl++){
+  for (let pl = 0; pl < poem.length; pl++) {
     let newLine = new PoemFlow(poem[pl], pl)
-    poemLines.push(newLine )
+    poemLines.push(newLine)
   }
 
   poemLines[0].color = color(186, 161, 48)
 
-  sliderOffsetY = -(200*(year()-2000))+1000;
+  sliderOffsetY = -(200 * (year() - 2000)) + 1000;
   console.log(sliderOffsetY)
 
 }
 
 
-function drawTickers(){
+function drawTickers() {
 
-  if(drawCount<1200){
+  if (drawCount < 1200) {
     return
   }
 
-let lineOpac = int(map(constrain(drawCount,1200,1500),1200,1500,0,200));
-let textOpac = int(map(constrain(drawCount,1400,1900),1400,1900,0,200));
+  let lineOpac = int(map(constrain(drawCount, 1200, 1500), 1200, 1500, 0, 200));
+  let textOpac = int(map(constrain(drawCount, 1400, 1900), 1400, 1900, 0, 200));
 
-// let lineOpac = 255
-// let textOpac = 255
+  // let lineOpac = 255
+  // let textOpac = 255
 
 
-textSize(14)
-for(let lineN = 0; lineN <200; lineN++){
-  if(lineN%2 == 0){
-  stroke(200, lineOpac)
-  strokeWeight(.5)
-  line(width-68, lineN*100, width, lineN*100)
-  noStroke()
-  fill(200, textOpac)
-  textAlign(LEFT);
-  yearN = 2000+floor(lineN/2)
-  // print(yearN)
+  textSize(14)
+  for (let lineN = 0; lineN < 200; lineN++) {
+    if (lineN % 2 == 0) {
+      stroke(200, lineOpac)
+      strokeWeight(.5)
+      line(width - 68, lineN * 100, width, lineN * 100)
+      noStroke()
+      fill(200, textOpac)
+      textAlign(LEFT);
+      yearN = 2000 + floor(lineN / 2)
+      // print(yearN)
 
-  text(str(yearN), width-68, (lineN*100) +textAscent())
-  textAlign(RIGHT);
-  if(timeData[str(yearN)]!=null){
-  text(timeData[str(yearN)][0]['title'], width-63-160, (lineN*100) + (textAscent()*3), 160)
-  }
+      text(str(yearN), width - 68, (lineN * 100) + textAscent())
+      textAlign(RIGHT);
+      if (timeData[str(yearN)] != null) {
+        text(timeData[str(yearN)][0]['title'], width - 63 - 160, (lineN * 100) + (textAscent() * 3), 160)
+      }
 
-  }else{
-    stroke(200, lineOpac)
-    strokeWeight(.5)
-    line(width-24, lineN*100, width, lineN*100)
+    } else {
+      stroke(200, lineOpac)
+      strokeWeight(.5)
+      line(width - 24, lineN * 100, width, lineN * 100)
 
-    if(timeData[str(yearN)]!=null){
-      if(timeData[str(yearN)].length>1){
-        noStroke();
-        fill(200, textOpac)
-        text(timeData[str(yearN)][1]['title'], width-63-160, (lineN*100) + (textAscent()*3), 160)
+      if (timeData[str(yearN)] != null) {
+        if (timeData[str(yearN)].length > 1) {
+          noStroke();
+          fill(200, textOpac)
+          text(timeData[str(yearN)][1]['title'], width - 63 - 160, (lineN * 100) + (textAscent() * 3), 160)
+        }
       }
     }
-  }
   }
 }
 
 function mouseWheel(event) {
   // print(pos);
   //move the square according to the vertical scroll amount
-  if(viewState>0){
-     pos += constrain(event.delta, -50, 50);
-  
-if(pos < sliderOffsetY){
-  pos = sliderOffsetY
-  }
-}
+  if (viewState > 0) {
+    pos += constrain(event.delta, -50, 50);
 
- 
+    if (pos < sliderOffsetY) {
+      pos = sliderOffsetY
+    }
+  }
+
+
 }
 
 // let lastPic = 0;
@@ -194,28 +194,28 @@ function draw() {
   drawCount++;
 
 
-// if( drawCount < 1000){
-//     let drifty = map(constrain(drawCount,00,2000),0,2000,height+100,0)
-//     let driftx = (100 + noise(drawCount/1000, drifty/1000)*100)
+  // if( drawCount < 1000){
+  //     let drifty = map(constrain(drawCount,00,2000),0,2000,height+100,0)
+  //     let driftx = (100 + noise(drawCount/1000, drifty/1000)*100)
 
-//     let opacT = constrain(drawCount,300,1000)
-//     opacT = map(opacT, 300,1000, 200,0)
+  //     let opacT = constrain(drawCount,300,1000)
+  //     opacT = map(opacT, 300,1000, 200,0)
 
-//     fill(186, 161, 48, opacT)
-//     textSize(24);
-//     textFont(fontRegular)
-//     text('whalefall',100+driftx,drifty)
-// }
-  
-textSize(24);
-textFont(fontRegular)
+  //     fill(186, 161, 48, opacT)
+  //     textSize(24);
+  //     textFont(fontRegular)
+  //     text('whalefall',100+driftx,drifty)
+  // }
 
-for (let pl = 0; pl < poemLines.length; pl++) {
-  let element = poemLines[pl];
-  if(element.PoemLine){
-    poemLines[pl].drawMove();
-  }
-}
+  textSize(24);
+  textFont(fontRegular)
+
+  // for (let pl = 0; pl < poemLines.length; pl++) {
+  //   let element = poemLines[pl];
+  //   if(element.PoemLine){
+  //     poemLines[pl].drawMove();
+  //   }
+  // }
 
 
   //   if(lastPic == 0){
@@ -230,50 +230,50 @@ for (let pl = 0; pl < poemLines.length; pl++) {
   //     imgCount++
   //   }
   // background(0);
-  
+
 
   // let tintAmt = map(pos, 0, -10000, 255, 0);
   // tint(255, tintAmt); // Apply transparency without
 
-let sliderGo = constrain(pos, sliderOffsetY, 100000)
+  let sliderGo = constrain(pos, sliderOffsetY, 100000)
 
-    if (sliderPos != sliderGo) {
+  if (sliderPos != sliderGo) {
     sliderPos += (sliderGo - sliderPos) / 15;
   }
 
 
-let WhaleGo = constrain(pos, -100, height-150)
-if(viewState== 0){
-  WhaleGo = pos;
-}
+  let WhaleGo = constrain(pos, -100, height - 150)
+  if (viewState == 0) {
+    WhaleGo = pos;
+  }
 
 
   if (whalePos != WhaleGo) {
-    whalePos += constrain( (WhaleGo - whalePos) / 250, -1,1);
+    whalePos += constrain((WhaleGo - whalePos) / 250, -1, 1);
   }
 
 
 
 
-  if(drawCount > 900 && viewState == 0){
-       pos = height-400
-      viewState++
-      console.log("viewState : " + viewState)
-    }
+  if (drawCount > 900 && viewState == 0) {
+    pos = height - 400
+    viewState++
+    console.log("viewState : " + viewState)
+  }
 
   // pos += 0.1;
-  image(Bimg, width / 2 - Bimg.width / 2,  whalePos);
+  image(Bimg, width / 2 - Bimg.width / 2, whalePos);
 
   push();
-  translate(width / 2 - Bimg.width / 2,  whalePos);
+  translate(width / 2 - Bimg.width / 2, whalePos);
   noStroke();
   showParticles();
   pop();
 
-push()
-translate(0, -sliderPos+sliderOffsetY)
- drawTickers()
- pop()
+  push()
+  translate(0, -sliderPos + sliderOffsetY)
+  drawTickers()
+  pop()
 
 }
 
@@ -306,17 +306,17 @@ class particle {
     rectMode(CENTER)
     this.timer++;
 
-    if(this.timer > 5000){
+    if (this.timer > 5000) {
       return
     }
 
     fill(this.color);
     push()
     translate(this.loc.x * 4, this.loc.y * 4)
-    let rotAmt = noise(this.loc.x/1000, this.loc.y/1000)*360
-    rotAmt *= map(constrain(this.timer,0,500),0,500,0,1)
+    let rotAmt = noise(this.loc.x / 1000, this.loc.y / 1000) * 360
+    rotAmt *= map(constrain(this.timer, 0, 500), 0, 500, 0, 1)
     rotate(rotAmt)
-    rect(0,0, 4.5, 4.5);
+    rect(0, 0, 4.5, 4.5);
     pop()
     if (this.timer > 10) {
       this.loc.y -= 0.075;
@@ -326,28 +326,28 @@ class particle {
 }
 
 
-class PoemFlow{
-  constructor(textLine, idx){
+class PoemFlow {
+  constructor(textLine, idx) {
     this.index = idx;
     this.PoemLine = textLine;
-    this.color = color(255,255,255)
+    this.color = color(255, 255, 255)
     this.locN = createVector(50, height);
 
-    let shiftAmmt = random(380,420)
-    this.range = [(idx)*shiftAmmt, ((idx)*shiftAmmt)+1000]
-    
-    if(idx = 0){
+    let shiftAmmt = random(380, 420)
+    this.range = [(idx) * shiftAmmt, ((idx) * shiftAmmt) + 1000]
+
+    if (idx = 0) {
       this.range[0] = -200
       this.range[1] = 2500
     }
   }
 
-  drawMove(){
+  drawMove() {
 
-    this.locN.y = map(constrain(drawCount,this.range[0],this.range[1]),this.range[0],this.range[1],height+100,0)
-    this.locN.x = (20+ noise(drawCount/500, this.locN.y/500)*200)
+    this.locN.y = map(constrain(drawCount, this.range[0], this.range[1]), this.range[0], this.range[1], height + 100, 0)
+    this.locN.x = (20 + noise(drawCount / 500, this.locN.y / 500) * 200)
 
-    let opacH = map(constrain(drawCount,this.range[0],this.range[1]),this.range[0],this.range[1],255,0)
+    let opacH = map(constrain(drawCount, this.range[0], this.range[1]), this.range[0], this.range[1], 255, 0)
     opacH = int(opacH)
     // opacT = map(opacT, this.range[0],this.range[1], 255*.8, 0)
     // let opacT = 255;
@@ -358,13 +358,13 @@ class PoemFlow{
     this.color.setAlpha(opacH)
     fill(this.color)
     // this.color.setAlpha(this.color.alpha-.5);
-    text( this.PoemLine, this.locN.x, this.locN.y)
+    text(this.PoemLine, this.locN.x, this.locN.y)
   }
 }
 
 
-class dataPoint{
-  constructor(){
+class dataPoint {
+  constructor() {
 
   }
 }
